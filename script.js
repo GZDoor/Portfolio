71c54b7f8b19d21b4d2d2a77cd3cec5d55c8051f
Thomas <Thomaschen14@gmail.com>
2021-02-09 16:25:17 -0800

    LMAO

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-
-
+$(document).ready(function(){
+    $(".box").on("mouseenter",function(){
+        $(this).animate({width:"50%",height:"200px"},300)
+    })
+    $(".box").on("mouseleave",function(){
+        $(this).animate({width:"40%",height:"100"},300)
+    })
+}) 
 
 
 
